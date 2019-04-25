0c59d516a8fbf29778bfa3b094fcf35b0aa1596e
Tom Morton <tom.m@38degrees.org.uk>
2019-04-25 12:30:24 +0100

    added a stateful root component, just to show how it's done

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,28 @@ import * as ReactDOM from "react-dom";
 
 import { Hello } from "./components/Hello";
 
+interface MainProps { name: string; }
+interface MainState { val: number; }
+
+class MainComponent extends React.Component<MainProps, MainState> {
+  constructor(props: Readonly<MainProps>) {
+    super(props);
+    this.state = {val: 0};
+  }
+  render() {
+    return <div>
+        <Hello compiler={this.props.name} framework="React" />
+        <p>{this.state.val}</p>
+        <button onClick={this.increment}>Press me</button>
+      </div>
+  }
+  increment = () => {
+    this.setState(state => ({val: this.state.val + 1}));
+  }
+}
+  
+
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("example")
+  <MainComponent name="Typescript 3.something" />,
+  document.getElementById("example")
 );
